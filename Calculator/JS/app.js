e22353a8eeb7011cc751ed5626b0c24fcae14354
Namiq Zeynalli <namiqzeynaalli@gmail.com>
2024-07-01 02:17:36 +0400

    Calculator app created

---
+++ Calculator/JS/app.js
@@ -0,0 +1,29 @@
+let inputBox = document.querySelector('#inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerText == "=") {
+            string = String(eval(string));
+            inputBox.value = string;
+        }
+        else if (e.target.innerText == "AC") {
+            string = "";
+            inputBox.value = string;
+        }
+        else if (e.target.innerText == "DEL") {
+            string = string.substring(0, string.length - 1);
+            inputBox.value = string;
+        }
+        else if (e.target.id == "opposite") {
+            string = String(-eval(string));
+            inputBox.value = string;
+        }
+        else {
+            string += e.target.innerText;
+            inputBox.value = string;
+        }
+    })
+})
